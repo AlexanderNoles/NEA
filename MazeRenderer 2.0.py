8361dcfdd604b30af68829d98d8c3b7abfca9fac
Alex <alexnoles@outlook.com>
2020-10-20 20:17:54 +0100

    Wall detection works, rendering revamp

--- MazeRenderer 2.0.py
+++ MazeRenderer 2.0.py
@@ -54,10 +54,11 @@ def play_maze(width,height,title,maze_data):
                     move_dir = "down"
                 else:
                     move_dir = ""
-                #Need to make it check to see if input is valid
-                print('')
-                print(player_pos[0],player_pos[1])
-                print(position_set_to[0] + add_to_x, position_set_to[1] + add_to_y)
+                if event.key == pygame.K_e: #This is a reset button and is only for testing, can be removed in final version
+                    first_frame = True
+                    screen.fill(default)
+                    from MazeGenerationNew import generate_random_walls
+                    maze_data = generate_random_walls(100,100)
                 #Checks to make sure player can't go outside the maze
                 if position_set_to[0] + add_to_x < 0 or position_set_to[0] + add_to_x > len(maze_data)-1:
                     add_to_x = 0
@@ -86,7 +87,7 @@ def play_maze(width,height,title,maze_data):
                 progress_bar(100,4,temp,screen,[0,0],[width,height]) #This is a stand-in progress bar to test how it would look, the real one's progression would be based on the maze generation progression
                 temp += 1 * delta_time                               #currently the maze generates before the progress bar is shown and the maze is rendered after (To make it properly linked to maze generation
         elif loading == False:                                       #the progress bar would have to be called from the MazeGenerationNew script) #len(maze_data[0])-2
-            if first_frame == True:
+            if first_frame == True:                
                 first_frame = False
                 generated_data = draw_maze([width,height],cube_size,screen,maze_data)
                 player_pos = draw_player(maze_data,generated_data[0],generated_data[1],cube_size,screen,[width,height],[0,0],True)
@@ -106,16 +107,12 @@ def wall(maze_data,position_to_set_to,player_pos,move_dir): #Checks to see if th
         "up":[2,3],
         "down":[3,2]
         }
-    print((maze_data[-player_pos[0]][-player_pos[1]]))
-    print((maze_data[-player_pos[0]][-player_pos[1]])[(dict_one[move_dir])[0]])
-    if (maze_data[-player_pos[0]][-player_pos[1]])[(dict_one[move_dir])[0]] == 1 and (maze_data[-player_pos[0]][-player_pos[1]])[(dict_one[move_dir])[0]] == 1:
-        print("wall")
+    if (maze_data[player_pos[1]][player_pos[0]])[(dict_one[move_dir])[0]] == 1 or (maze_data[position_to_set_to[1]][position_to_set_to[0]])[(dict_one[move_dir])[1]] == 1:
         return True
     else:
-        print("no wall")
         return False
 
-def draw_maze(window_dimensions,cube_size,screen,maze_data):    
+def draw_maze(window_dimensions,cube_size,screen,maze_data):
     #This function will draw the maze based on maze data generated in the maze generation script
     #A cube_size of two and below will cause the squares to be too small to be properly represented properly on any pixelated screen, hence, 3 is the lowest the function allows
     if cube_size < 3: cube_size = 3 
@@ -126,12 +123,12 @@ def draw_maze(window_dimensions,cube_size,screen,maze_data):
     offsetx = 0
     for y in range(0,len(maze_data)):
         for x in range(0, len(maze_data[y])):
-            draw_rectangle([int(0-(maze_height/2)+offsetx),int((0-maze_width/2)+offsety)],cube_size,cube_size,False,white,screen,window_dimensions,maze_data[y][x])
-            offsetx += cube_size-1       
-        offsety += cube_size-1  
+            draw_rectangle([(0-(maze_height/2)+offsetx),((0-maze_width/2)+offsety)],cube_size,cube_size,False,white,screen,window_dimensions,maze_data[y][x])
+            offsetx += cube_size-1    
+        offsety += cube_size-1
         offsetx = 0
     #Maze Shell#
-    #draw_outline([int(0-maze_height/2),int(0-maze_width/2)],((cube_size-1)*len(maze_data[0])),((cube_size-1)*len(maze_data)),white,screen,window_dimensions,[1,1,1,1])
+    #draw_rectangle([(0-maze_height/2),(0-maze_width/2)],((cube_size-1)*len(maze_data[0])),((cube_size-1)*len(maze_data)),False,white,screen,window_dimensions,[1,1,1,1]) #Currently the size of the maze shell is drawn wrong so it is disabled
     return [maze_width,maze_height,]
 
 def draw_player(maze_data,maze_height,maze_width,cube_size,screen,window_dimensions,new_pos,first_pos,*args):
@@ -152,7 +149,7 @@ def draw_player(maze_data,maze_height,maze_width,cube_size,screen,window_dimensi
 def find_center_of_square(maze_width,maze_height,cube_size,pos): #Converts map coords to pixel measurments so that the player can be properly positioned 
     x = pos[0]                                                   #Currently while the player isn't intersecting walls they don't look like they are in the center 
     y = pos[1]
-    pos_in_pixels = [int(0-(maze_width/2)+(cube_size-1)*x)+(0.2*cube_size),int(0-(maze_height/2)+(cube_size-1)*y)+(0.2*cube_size)]
+    pos_in_pixels = [(0-(maze_width/2)+(cube_size-1)*x)+(0.2*cube_size),(0-(maze_height/2)+(cube_size-1)*y)+(0.2*cube_size)]
     #print(pos_in_pixels)   
     return pos_in_pixels
 
@@ -163,37 +160,29 @@ def progress_bar(width,number_of_segements,progress,screen,offset,window_dimensi
     #Draw actual progress
     draw_rectangle([int(center[0]-width/2)+2,center[1]+2],int((width-4)*progress),11,True,white,screen,window_dimensions)
 
-#Both draw_outline and draw_rectangle could be combined down into a just draw_rectangle (however that can be done at a later date)
-def draw_outline(startpos,width,height,colour,screen,window_dimensions,list_one):
-    width = alter_to_fit_scale(width) 
-    height = alter_to_fit_scale(height)
-    for i in range(0,len(startpos)):
-        startpos[i] = alter_coords_to_fit_scale(startpos[i],int((window_dimensions[i])/2))
-    for x in range(startpos[0],startpos[0]+width):
-        for y in range(startpos[1],startpos[1]+height):
-            if (x in range(startpos[0]+list_one[0],startpos[0]+width-list_one[1])) and (y in range(startpos[1]+list_one[2],startpos[1]+height-list_one[3])):
-                pass
-            else:
-                screen.set_at((x,y),colour)
-
 def draw_rectangle(startpos,width,height,fill,colour,screen,window_dimensions,*args):
     #Draws a rectangle based around 0,0 (screen center), for best use don't redraw things that are already drawn (i.e. stagnant sprites, like the maze)
     width = alter_to_fit_scale(width)
     height = alter_to_fit_scale(height)
     for i in range(0,len(startpos)):
         startpos[i] = alter_coords_to_fit_scale(startpos[i],int((window_dimensions[i])/2))
-    for x in range(startpos[0],startpos[0]+width):
-        for y in range(startpos[1],startpos[1]+height):
-            screen.set_at((x,y),colour)
+    if fill:
+        for x in range(startpos[0],startpos[0]+width):
+            for y in range(startpos[1],startpos[1]+height):
+                screen.set_at((x,y),colour)
     if not fill:
-        try:  #This allows rectangles to not be filled with the weight of thier lines controlled by args[0] (if it's entered as an int)
-            for x in range(startpos[0]+args[0],startpos[0]+width-args[0]):
-                for y in range(startpos[1]+args[0],startpos[1]+height-args[0]):
-                    screen.set_at((x,y),default)
-        except: #Allows different sides of the cube to have different line weights controlled by args[0] (if it's entered as a list of ints)
-            for x in range(startpos[0]+args[0][0],startpos[0]+width-args[0][1]):
-                for y in range(startpos[1]+args[0][2],startpos[1]+height-args[0][3]):
-                    screen.set_at((x,y),default)
+        list_one = []
+        if isinstance(args[0], int):
+            for i in range(0,4):
+                list_one.append(args[0])
+        else:
+            list_one = args[0]
+        for x in range(startpos[0],startpos[0]+width):
+            for y in range(startpos[1],startpos[1]+height):
+                if (x in range(startpos[0]+list_one[0],startpos[0]+width-list_one[1])) and (y in range(startpos[1]+list_one[2],startpos[1]+height-list_one[3])):
+                    pass
+                else:
+                    screen.set_at((x,y),colour)
 
 def alter_to_fit_scale(value):
     #Alters any variable to fit game scale
